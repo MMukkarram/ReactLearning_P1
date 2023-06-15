a54adae908b7ce3c5c2a45e9c9e2e75c023fd1eb
Muhammad Mukkarram <mukkarram.moin@venturedive.com>
2023-06-15 05:34:23 +0500

    ExpenseProjectCompleted_AndModule_5 completed as well

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -3,7 +3,6 @@ import React from "react";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-
   const onSelectFilterYearHandler = (event) => {
     props.onChangeFilter(event.target.value);
   };
@@ -13,6 +12,7 @@ const ExpensesFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={props.selected} onChange={onSelectFilterYearHandler}>
+          <option value="2023">2023</option>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
